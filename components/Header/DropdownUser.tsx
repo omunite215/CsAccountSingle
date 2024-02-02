2faa697e03d7b9a10bff0cf62830e3a22984b43c
Om Patel <omunite21@gmail.com>
2024-02-02 13:38:18 +0530

    update header refresh

--- components/Header/DropdownUser.tsx
+++ components/Header/DropdownUser.tsx
@@ -36,11 +36,10 @@ const DropdownUser = () => {
 
   return (
     <div className="relative">
-      <Link
+      <div
         ref={trigger}
         onClick={() => setDropdownOpen(!dropdownOpen)}
         className="flex items-center gap-4"
-        href="#"
       >
         <span className="hidden text-right lg:block">
           <span className="block text-sm font-medium text-black dark:text-white">
@@ -73,7 +72,7 @@ const DropdownUser = () => {
             fill=""
           />
         </svg>
-      </Link>
+      </div>
 
       {/* <!-- Dropdown Start --> */}
       <div
