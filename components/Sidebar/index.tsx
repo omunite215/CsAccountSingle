39a70d133a47beec6f6a14195f50deffc5cff23c
Om Patel <omunite21@gmail.com>
2024-02-02 12:38:01 +0530

    update cards

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -102,7 +102,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
           {/* <!-- Menu Group --> */}
           <div>
             <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
-              MENU
+              ACCOUNT DASHBOARD
             </h3>
 
             <ul className="mb-6 flex flex-col gap-1.5">
